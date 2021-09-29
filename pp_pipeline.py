da3a6897a4c120dcb0b68c0b7daf36568f57c131
dimaleyko <dimaleyko@gmail.com>
2021-09-29 19:12:29 +0100

    Add comments

--- pp_pipeline.py
+++ pp_pipeline.py
@@ -12,21 +12,26 @@ dict_keys = ['Transaction unique identifier', 'Price', 'Date of Transfer', 'Post
 
 def main(argv = None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', dest = 'input', default = '../pp-shortened.csv', help = 'Input file for the pipeline') #../pp-complete.csv
+    parser.add_argument('--input', dest = 'input', default = '../pp-shortened.csv', help = 'Input file for the pipeline')#../pp-complete.csv
     parser.add_argument('--output', dest = 'output', default = '../pp-transformed.ndjson', help = 'Output file for the pipeline')
     known_args = parser.parse_args(argv)
 
     class AddPropertyID(beam.DoFn):
         def process(self, data_element):
+            #Splitting data_element into an itterable list
             for line in csv.reader([data_element], quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True):
-                hash_property_id = hashlib.sha1(str.encode(', '.join(line[7:14] + [line[3]]))).hexdigest()                                                                                                                                                             
-                data_element = dict(zip(dict_keys, line))                                                                            
+                #Assuming that address fields do not change for the same property throughout the years,
+                #generating unique hash based on the adress string to use as Property ID      
+                hash_property_id = hashlib.sha1(str.encode(', '.join(line[7:14] + [line[3]]))).hexdigest()                          
+                #Adding keys to the data for ease of json conversion at a later point                                                                                                                                                                                                                                                
+                data_element = dict(zip(dict_keys, line))                                                                                                                                                 
             yield hash_property_id, data_element
 
     class JSONCreate(beam.DoFn):
         def process(self, data_element):
             hash_property_id, data_element = data_element
-            combined_data_json_object = {'PropertyID': hash_property_id, 'Transactions': data_element}                                                
+            #Creating a nested dictionary to later convert to a properly formated json
+            combined_data_json_object = {'PropertyID': hash_property_id, 'Transactions': data_element}                                                                       
             return [json.dumps(combined_data_json_object)]
 
     with beam.Pipeline() as p:

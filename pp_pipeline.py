c4cbb59e9d3ea7a68cb060fcc7b5349539cbcdc8
dimaleyko <dimaleyko@gmail.com>
2021-09-30 08:30:19 +0100

    Change default input directory

--- pp_pipeline.py
+++ pp_pipeline.py
@@ -12,7 +12,7 @@ dict_keys = ['Transaction unique identifier', 'Price', 'Date of Transfer', 'Post
 
 def main(argv = None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', dest = 'input', default = '../pp-shortened.csv', help = 'Input file for the pipeline')#../pp-complete.csv
+    parser.add_argument('--input', dest = 'input', default = '../pp-complete.csv', help = 'Input file for the pipeline')
     parser.add_argument('--output', dest = 'output', default = '../pp-transformed.ndjson', help = 'Output file for the pipeline')
     known_args = parser.parse_args(argv)
 
